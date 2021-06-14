138736adcc65fb8e2cc338fe19facec837ca417b
FabioUmpierre <Fabio.umpierre@yahoo.com>
2021-06-14 12:04:10 -0300

    atualizado

--- db/create_table_advantages.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE kanto . advantage(
-id BIGINT(20) NOT NULL AUTO_INCREMENT,
-name VARCHAR(45) NOT NULL,
-PRIMARY KEY(id));
-
-INSERT INTO advantage(name) VALUES('Attack2x');
-INSERT INTO advantage(name) VALUES('Attack1x');
-INSERT INTO advantage(name) VALUES('Attack1/2x');
-INSERT INTO advantage(name) VALUES('Noeffect');
