a5c7a7d2f3425ddd1fbbff3a6815eaa80f2c7b09
FabioUmpierre <Fabio.umpierre@yahoo.com>
2021-06-21 14:45:20 -0300

    PR cleanups

--- teste.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open(r"C:\Users\Pichau\dev\banco\typescsv.csv", 'r+') as ficheiro:
-    reader = csv.reader(ficheiro)
-    lista = []
-    for i in reader:
-        if i != 'Unnamed: 0':
-           lista.appen
-         
